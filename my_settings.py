ef1ea37507f7d71ed563f87077c1743b0fab07d5
맥북 <mac@maegbug-ui-MacBookPro.local>
2021-07-19 11:16:36 +0900

    first

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-pi%gd#outg+$9w9k^k_lmljn*0&(_9o0-809z#!9%cy^q(30z)'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'apt',
+        'USER': 'root',
+        'PASSWORD': 'qwe123!@#',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
